4ab0a436eaf6d1ebd50f3272f5f1d1dfce0f209a
deepak-response <80972988+deepak-response@users.noreply.github.com>
2023-12-07 00:52:53 +0530

    controller changes

--- src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
+++ src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
@@ -1,5 +1,6 @@
 package com.javatechie.spring.drools.api;
 
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,21 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class MegaOfferController {
 	@Autowired
 	private MegaOfferService megaOfferService;
-
+	@Autowired
+	private KieContainer kieContainer;
 	@Autowired
 	private KieSession session;
 
 	@PostMapping("/order")
 	public Order orderNow(@RequestBody Order order) {
+		Order orderDiscount = new Order();
+		KieSession kieSession = kieContainer.newKieSession();
+		kieSession.setGlobal("orderDiscount", orderDiscount);
 		session.insert(order);
 		session.fireAllRules();
+		//session.dispose();
 		return order;
 	}
+
 
 	@PostMapping("/get-discount")
 	public ResponseEntity<Order> getDiscount(@RequestBody Order orderRequest) {
 		Order discount = megaOfferService.getDiscount(orderRequest);
-		return new ResponseEntity<>(discount, HttpStatus.OK);
+		return new ResponseEntity<>(orderRequest, HttpStatus.OK);
 	}
 
 }
